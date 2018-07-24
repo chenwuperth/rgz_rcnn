0a636bfa8330f405949b7b8187b3fbc5210f9722
Chen Wu <chen.wu@uwa.edu.au>
2018-07-24 15:12:41 +0800

    split squre images out of the mosaics working

--- tools/fits_utils.py
+++ tools/fits_utils.py
@@ -93,7 +93,8 @@ def clip_file(fname):
     file.close()
 
 def split_file(fname, width_ratio, height_ratio, halo_ratio=50,
-               show_split_scheme=False, work_dir='/tmp'):
+               show_split_scheme=False, work_dir='/tmp', 
+               equal_aspect=False):
     """
     width_ratio = current_width / new_width, integer
     height_ratio = current_height / new_height, integer
@@ -101,29 +102,34 @@ def split_file(fname, width_ratio, height_ratio, halo_ratio=50,
     """
     file = pyfits.open(fname)
     d = file[0].data
-    fhead = file[0].header
+    #fhead = file[0].header
     h = d.shape[-2] #y
     w = d.shape[-1] #x
     print(h, w)
-    new_h = int(h / height_ratio)
-    extra_h = h % height_ratio
-    halo_h = new_h / halo_ratio
-    #print(extra_h)
-    ny = np.arange(height_ratio) * new_h# + (new_h / 2)
-    #ny[-1] += extra_h / 2
+    if (equal_aspect):
+        size = int(min(h / height_ratio, w / width_ratio))
+        halo_h, halo_w = (size / halo_ratio,) * 2
+        height_ratio, width_ratio = h / size, w / size # update the ratio just in case
+        ny = np.arange(height_ratio) * size
+        nx = np.arange(width_ratio) * size
+        if (ny[-1] + size + halo_h < h):
+            ny = np.hstack([ny, h - size])
+        if (nx[-1] + size + halo_w < w):
+            nx = np.hstack([nx, w - size])
+        new_w, new_h = (size,) * 2
+    else:
+        new_h = int(h / height_ratio)
+        halo_h = new_h / halo_ratio
+        ny = np.arange(height_ratio) * new_h
+        new_w = int(w / width_ratio)
+        halo_w = new_w / halo_ratio
+        nx = np.arange(width_ratio) * new_w
+    print(new_h, new_w)
     print(ny)
-    new_w = int(w / width_ratio)
-    extra_w = w % width_ratio
-    halo_w = new_w / halo_ratio
-    #print(extra_w)
-    nx = np.arange(width_ratio) * new_w# + (new_w / 2)
-    #nx[-1] += extra_w / 2
     print(nx)
-    # xx, yy = np.meshgrid(nx, ny)
-    # print(xx[1])
-    # print(yy[1])
+
     if (show_split_scheme):
-        fig, ax = plt.subplots(1)
+        _, ax = plt.subplots(1)
         ax.imshow(np.reshape(d, [d.shape[-2], d.shape[-1]]))
 
     for i, x in enumerate(nx):
@@ -132,28 +138,27 @@ def split_file(fname, width_ratio, height_ratio, halo_ratio=50,
             y1 = max(y - halo_w, 0)
             wd = new_w
             hd = new_h
-            if (i == len(nx) - 1):
-                x2 = w - 1
-            else:
-                x2 = min(x1 + wd + halo_w, w - 1)
-            if (j == len(ny) - 1):
-                y2 = h - 1
-            else:
-                y2 = min(y1 + hd + halo_h, h - 1)
+            
+            x2_c = x1 + wd + halo_w
+            x2 = min(x2_c, w - 1)
+            x1 -= max(0, x2_c - (w - 1))
+                
+            y2_c = y1 + hd + halo_h
+            y2 = min(y2_c, h - 1)
+            y1 -= max(0, y2_c - (h - 1))
             fid = osp.basename(fname).replace('.fits', '%d-%d.fits' % (i, j))
-            #cmd = cutout_cmd % (fid, work_dir, fname, ra0, dec0, width, height)
             out_fname = osp.join(work_dir, fid)
             print(splitimg_cmd % (fname, out_fname, x1, y1, (x2 - x1), (y2 - y1)))
-            #print(x1, y1, ' ', x2, y2)
             if (show_split_scheme):
                 rect = patches.Rectangle((x1, y1), (x2 - x1), (y2 - y1),
-                                        linewidth=1, edgecolor='r',
+                                        linewidth=0.5, edgecolor='r',
                                         facecolor='none')
                 # Add the patch to the Axes
                 ax.add_patch(rect)
     if (show_split_scheme):
+        plt.tight_layout()
         plt.show()
-        plt.savefig('test.png')
+        plt.savefig('test.pdf')
 
 if __name__ == '__main__':
     #root_dir = '/Users/Chen/proj/rgz-ml/data/EMU_GAMA23'
@@ -162,4 +167,4 @@ if __name__ == '__main__':
     # clip_file(fname)
 
     fname = osp.join(root_dir, 'gama_linmos_corrected_clipped.fits')
-    split_file(fname, 12, 12)
+    split_file(fname, 12, 12, show_split_scheme=True, equal_aspect=True)
